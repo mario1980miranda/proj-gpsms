156c1f64f1a063ede88d52337befb1634a29b9b2
Mario Miranda <mario_chan15@hotmail.com>
2022-09-03 14:59:57 -0400

    Iniciando o projeto Authuser

---
+++ authuser/src/main/java/com/codetruck/authuser/enums/UserStatus.java
@@ -0,0 +1,7 @@
+package com.codetruck.authuser.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    BLOCKED,
+    INACTIVE
+}
---
+++ authuser/src/main/java/com/codetruck/authuser/enums/UserType.java
@@ -0,0 +1,6 @@
+package com.codetruck.authuser.enums;
+
+public enum UserType {
+	WIZARD,
+	ADMIN
+}
