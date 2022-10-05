6b25c6b7b7fa4ba43709e6ae09137e8a0dd04ecd
Mario Miranda <mario_chan15@hotmail.com>
2022-10-05 10:39:42 -0400

    GPS-MS - Service Registry & Api Gateway

---
+++ service-registry/src/test/java/com/codetruck/gps/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetruck.gps.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- typification/src/main/java/com/codetruck/typification/TypificationApplication.java
+++ typification/src/main/java/com/codetruck/typification/TypificationApplication.java
@@ -2,8 +2,10 @@ package com.codetruck.typification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TypificationApplication {
 
 	public static void main(String[] args) {
